10adf71d7db4f80d4547338d111c28782675e3b1
Jia-Shen Boon <jiashen@gmail.com>
2021-07-28 07:31:50 +0900

    Add skeleton environment
    __Testing done__
    
    `cdk deploy` succeeds

--- lib/photomosaic-infra-stack.ts
+++ lib/photomosaic-infra-stack.ts
@@ -2,10 +2,36 @@ import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3'
 import * as elasticbeanstalk from '@aws-cdk/aws-elasticbeanstalk'
 
+// Reference:
+// https://github.com/aws-samples/aws-cdk-examples/blob/master/typescript/elasticbeanstalk/elasticbeanstalk-environment/index.ts
 export class PhotomosaicInfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     new s3.Bucket(this, "MyBucket", {});
-    const mosaicApp = new elasticbeanstalk.CfnApplication(this, "mosaicApp", {});
+    const mosaicAppName = "MosaicApp";
+    const mosaicApp = new elasticbeanstalk.CfnApplication(this, "mosaicApp", {
+      applicationName: mosaicAppName,
+    });
+
+    // Taken from
+    // https://medium.com/@joshmustill/complete-node-js-aws-elastic-beanstalk-application-packaging-through-cdk-in-typescript-e91b7ffe4928
+    const mosaicEnv = new elasticbeanstalk.CfnEnvironment(this, "mosaicEnv", {
+      applicationName: mosaicAppName,
+      optionSettings: [
+        {
+          namespace: 'aws:autoscaling:launchconfiguration',
+          optionName: 'IamInstanceProfile',
+          // This role seems to be the default role
+          // See https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/concepts-roles-instance.html
+          // Without this option setting, deployment fails with:
+          // Environment failed to launch as it entered Terminated state
+          // Environment must have instance profile associated with it.
+          value: 'aws-elasticbeanstalk-ec2-role',
+        },
+      ],
+      solutionStackName: "64bit Amazon Linux 2 v3.3.3 running Python 3.8",
+    });
+
+    mosaicEnv.addDependsOn(mosaicApp)
   }
 }
