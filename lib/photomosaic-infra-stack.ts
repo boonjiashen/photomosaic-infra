628ff71e36c154dc4fd902c732a9d4b9dc718483
Jia-Shen Boon <jiashen@gmail.com>
2021-09-17 07:55:10 +0900

    Add placeholder API for image processor
    __Testing done__
    
    1. `cdk deploy` succeeds
    1. <https://jdkr8d9qs9.execute-api.ap-northeast-1.amazonaws.com/prod/> shows `{"message": "Internal server error"}`

--- lib/photomosaic-infra-stack.ts
+++ lib/photomosaic-infra-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'monocdk';
 import * as s3 from 'monocdk/aws-s3';
 import * as s3deploy from 'monocdk/aws-s3-deployment';
+import * as lambda from 'monocdk/aws-lambda';
+import * as apig from 'monocdk/aws-apigateway';
 
 export class PhotomosaicInfraStack extends cdk.Stack {
 
@@ -16,6 +18,14 @@ export class PhotomosaicInfraStack extends cdk.Stack {
       sources: [s3deploy.Source.asset('./assets')],
       destinationBucket: siteBucket,
       retainOnDelete: false,
+    });
+
+    new apig.LambdaRestApi(this, 'appApi', {
+      handler: new lambda.Function(this, "app", {
+        runtime: lambda.Runtime.NODEJS_14_X,
+        handler: "widgets.main",
+        code: lambda.Code.fromInline("not real code"),
+      }),
     });
   }
 }
