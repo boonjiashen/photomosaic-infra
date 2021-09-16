5acc1e1d52ecae451a503f2c60e9ca21fab7ed0f
Jia-Shen Boon <jiashen@gmail.com>
2021-09-17 07:43:08 +0900

    Create static website
    __Testing done__
    
    1. `cdk deploy` succeeds
    1. <http://photomosaicinfrastack-mybucketf68f3ff0-n94gz3ko0lca.s3-website-ap-northeast-1.amazonaws.com/> displays static website

--- lib/photomosaic-infra-stack.ts
+++ lib/photomosaic-infra-stack.ts
@@ -1,11 +1,21 @@
 import * as cdk from 'monocdk';
-import * as s3 from 'monocdk/aws-s3'
+import * as s3 from 'monocdk/aws-s3';
+import * as s3deploy from 'monocdk/aws-s3-deployment';
 
 export class PhotomosaicInfraStack extends cdk.Stack {
 
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, "MyBucket", {});
+    const siteBucket = new s3.Bucket(this, "MyBucket", {
+      publicReadAccess: true,
+      websiteIndexDocument: "index.html",
+    });
+
+    new s3deploy.BucketDeployment(this, 'deployStaticPage', {
+      sources: [s3deploy.Source.asset('./assets')],
+      destinationBucket: siteBucket,
+      retainOnDelete: false,
+    });
   }
 }
