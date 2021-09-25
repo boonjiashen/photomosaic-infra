31973025a33498c4ea82e08e49aee0f7f4720821
Jia-Shen Boon <jiashen@gmail.com>
2021-09-25 09:06:43 +0900

    Move site to personal domain
    Previously the site was on a random S3 bucket. Now we move it to mosaic[.dev].boonjiashen.com
    
    __Testing done__
    
    1. `cdk deploy`
    1. Tested in mosaic.dev.boonjiashen.com

--- TODO.md
+++ TODO.md
@@ -1,4 +1,5 @@
 * Refactor io.BytesIO with context manager and autoclose
 * How to call APIG locally from the client side? Maybe https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-cdk-getting-started.html
 * Handle case when fetch fails
-* Move site to mosaic.boonjiashen.ccom
+* Deploy through pipeline
+* add project back to blog
--- lib/photomosaic-infra-stack.ts
+++ lib/photomosaic-infra-stack.ts
@@ -6,16 +6,50 @@ import * as lambda from 'monocdk/aws-lambda';
 import * as apig2 from 'monocdk/aws-apigatewayv2';
 import * as apig2_integrations from 'monocdk/aws-apigatewayv2-integrations';
 import * as iam from 'monocdk/aws-iam';
-import * as logs from 'monocdk/aws-logs'
+import * as logs from 'monocdk/aws-logs';
+import * as route53 from 'monocdk/aws-route53';
+import * as route53Targets from 'monocdk/aws-route53-targets';
 
 export class PhotomosaicInfraStack extends cdk.Stack {
 
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, {
+      env: {
+        // Cannot use an S3 record alias in region-agnostic stack
+        region: 'ap-northeast-1',
+      },
+    });
 
-    const siteBucket = new s3.Bucket(this, "MyBucket", {
+    const devhostedZoneAttr = {
+      hostedZoneId: "Z05586802LBP65122KD61",
+      zoneName: "dev.boonjiashen.com",
+    };
+    const prodHostedZoneAttr = {
+      hostedZoneId: "Z09544292C54AJ4VKFFSY",
+      zoneName: "boonjiashen.com",
+    };
+    const hostedZoneAttr = devhostedZoneAttr;
+    // If you need to the hostname from the HostedZone object we need to use
+    // `fromHostedZoneAttributes` rather than the simpler methods like `fromHostedZoneId`
+    // See https://github.com/aws/aws-cdk/issues/3663
+    const hostedZone = route53.HostedZone.fromHostedZoneAttributes(this, "hostedZoned", hostedZoneAttr);
+
+    const siteBucket = new s3.Bucket(this, 'siteBucket', {
+      bucketName: 'mosaic.' + hostedZone.zoneName,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true, // deletes bucket even if non-empty
+      versioned: true,
       publicReadAccess: true,
-      websiteIndexDocument: "index.html",
+      // Also enables static website hosting
+      websiteIndexDocument: 'index.html',
+    });
+
+    const siteRecord = new route53.ARecord(this, 'siteRecord', {
+      zone: hostedZone,
+      recordName: 'mosaic',
+      target: route53.RecordTarget.fromAlias(
+        new route53Targets.BucketWebsiteTarget(siteBucket)
+      ),
     });
 
     new s3deploy.BucketDeployment(this, 'deployStaticPage', {
@@ -82,7 +116,7 @@ export class PhotomosaicInfraStack extends cdk.Stack {
     };
 
     new cdk.CfnOutput(this, "siteUrl", {
-      value: siteBucket.bucketWebsiteUrl,
+      value: siteRecord.domainName,
     });
 
     new cdk.CfnOutput(this, "appHttpApiEndpoint", {
