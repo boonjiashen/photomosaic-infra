a83cdc26f8d757cafc132d07ccc9762aa855075a
Jia-Shen Boon <jiashen@gmail.com>
2021-10-02 07:36:44 +0900

    Move all imports to cdk
    To save the time manually creating imports, since we depend on the top
    level monocdk package anyway
    
    __Testing done__
    
    `cdk deploy beta`

--- lib/infra-stack.ts
+++ lib/infra-stack.ts
@@ -1,21 +1,7 @@
 import * as cdk from 'monocdk';
-import * as s3 from 'monocdk/aws-s3';
-import * as s3deploy from 'monocdk/aws-s3-deployment';
-import * as lambda from 'monocdk/aws-lambda';
-
-import * as acm from 'monocdk/aws-certificatemanager';
-import * as apig2 from 'monocdk/aws-apigatewayv2';
-import * as apig2_integrations from 'monocdk/aws-apigatewayv2-integrations';
-import * as cf from 'monocdk/aws-cloudfront';
-import * as cf_origins from 'monocdk/aws-cloudfront-origins';
-import * as iam from 'monocdk/aws-iam';
-import * as logs from 'monocdk/aws-logs';
-import * as route53 from 'monocdk/aws-route53';
-import * as route53Targets from 'monocdk/aws-route53-targets';
-import { URL } from 'url';
 
 interface InfraStackProps extends cdk.StackProps {
-  hostedZoneAttr: route53.HostedZoneAttributes
+  hostedZoneAttr: cdk.aws_route53.HostedZoneAttributes
 }
 
 export class InfraStack extends cdk.Stack {
@@ -26,12 +12,12 @@ export class InfraStack extends cdk.Stack {
     // If you need to the hostname from the HostedZone object we need to use
     // `fromHostedZoneAttributes` rather than the simpler methods like `fromHostedZoneId`
     // See https://github.com/aws/aws-cdk/issues/3663
-    const hostedZone = route53.HostedZone.fromHostedZoneAttributes(this, "hostedZone", props.hostedZoneAttr);
+    const hostedZone = cdk.aws_route53.HostedZone.fromHostedZoneAttributes(this, "hostedZone", props.hostedZoneAttr);
 
     const siteDomainPrefix = "mosaic"
     const siteDomainName = `${siteDomainPrefix}.${hostedZone.zoneName}`
     const websiteRootDoc = 'index.html'
-    const siteBucket = new s3.Bucket(this, 'siteBucket', {
+    const siteBucket = new cdk.aws_s3.Bucket(this, 'siteBucket', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true, // deletes bucket even if non-empty
       versioned: true,
@@ -40,52 +26,52 @@ export class InfraStack extends cdk.Stack {
       websiteIndexDocument: websiteRootDoc,
     });
 
-    new s3deploy.BucketDeployment(this, 'deployStaticPage', {
-      sources: [s3deploy.Source.asset('./assets/client')],
+    new cdk.aws_s3_deployment.BucketDeployment(this, 'deployStaticPage', {
+      sources: [cdk.aws_s3_deployment.Source.asset('./assets/client')],
       destinationBucket: siteBucket,
       retainOnDelete: false,
     });
 
-    const siteCertificate = new acm.DnsValidatedCertificate(this, 'siteCertificate', {
+    const siteCertificate = new cdk.aws_certificatemanager.DnsValidatedCertificate(this, 'siteCertificate', {
       domainName: siteDomainName,
       hostedZone,
     });
 
-    const siteDistribution = new cf.Distribution(this, 'siteDistribution', {
+    const siteDistribution = new cdk.aws_cloudfront.Distribution(this, 'siteDistribution', {
       defaultBehavior: {
-        origin: new cf_origins.S3Origin(siteBucket),
-        viewerProtocolPolicy: cf.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-        cachePolicy: cf.CachePolicy.CACHING_DISABLED,
+        origin: new cdk.aws_cloudfront_origins.S3Origin(siteBucket),
+        viewerProtocolPolicy: cdk.aws_cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: cdk.aws_cloudfront.CachePolicy.CACHING_DISABLED,
       },
       certificate: siteCertificate,
       domainNames: [siteDomainName],
       defaultRootObject: websiteRootDoc,
     });
 
-    const siteRecord = new route53.ARecord(this, 'siteRecord', {
+    const siteRecord = new cdk.aws_route53.ARecord(this, 'siteRecord', {
       zone: hostedZone,
       recordName: siteDomainPrefix,
-      target: route53.RecordTarget.fromAlias(
-        new route53Targets.CloudFrontTarget(siteDistribution),
+      target: cdk.aws_route53.RecordTarget.fromAlias(
+        new cdk.aws_route53_targets.CloudFrontTarget(siteDistribution),
       ),
     });
 
-    const appFunction = new lambda.DockerImageFunction(this, "app", {
-      code: lambda.DockerImageCode.fromImageAsset("./assets/service/processor_lambda"),
+    const appFunction = new cdk.aws_lambda.DockerImageFunction(this, "app", {
+      code: cdk.aws_lambda.DockerImageCode.fromImageAsset("./assets/service/processor_lambda"),
       timeout: cdk.Duration.seconds(90),
       memorySize: 10240,
-      tracing: lambda.Tracing.ACTIVE,
+      tracing: cdk.aws_lambda.Tracing.ACTIVE,
     });
     appFunction.role?.addManagedPolicy(
-      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonS3ReadOnlyAccess")
+      cdk.aws_iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonS3ReadOnlyAccess")
     )
 
-    const appHttpApi = new apig2.HttpApi(this, "appHttpApi", {
+    const appHttpApi = new cdk.aws_apigatewayv2.HttpApi(this, "appHttpApi", {
       description: `API for ${props.stackName}`,
       corsPreflight: {
         // allowMethods and allowOrigins to avoid this browser error during preflight
         // `No 'Access-Control-Allow-Origin' header is present on the requested resource.`
-        allowMethods: [apig2.CorsHttpMethod.ANY],
+        allowMethods: [cdk.aws_apigatewayv2.CorsHttpMethod.ANY],
         allowOrigins: ["*"],
         // allowHeaders to avoid this browser error during preflight
         // `Request header field content-type is not allowed by Access-Control-Allow-Headers in preflight response`
@@ -94,7 +80,7 @@ export class InfraStack extends cdk.Stack {
     });
     appHttpApi.addRoutes({
       path: "/",
-      integration: new apig2_integrations.LambdaProxyIntegration({
+      integration: new cdk.aws_apigatewayv2_integrations.LambdaProxyIntegration({
         handler: appFunction,
       }),
     });
@@ -102,7 +88,7 @@ export class InfraStack extends cdk.Stack {
     // HttpApi L2 construct doesn't support logging yet.
     // Instructions on opening escape hatch in:
     // https://github.com/aws/aws-cdk/issues/11100#issuecomment-782213423
-    const log = new logs.LogGroup(this, "appHttpApiLogGroup");
+    const log = new cdk.aws_logs.LogGroup(this, "appHttpApiLogGroup");
     const logFormat = {
       "requestId": "$context.requestId",
       "ip": "$context.identity.sourceIp",
@@ -123,7 +109,7 @@ export class InfraStack extends cdk.Stack {
         "responseType": "$context.error.responseType",
       }
     };
-    (appHttpApi.defaultStage?.node.defaultChild as apig2.CfnStage)
+    (appHttpApi.defaultStage?.node.defaultChild as cdk.aws_apigatewayv2.CfnStage)
       .accessLogSettings = {
       destinationArn: log.logGroupArn,
       format: JSON.stringify(logFormat),
